2306a0f30e0f942f7691c7daa11942b1888caa19
marco76tv <marco.sottana@gmail.com>
2024-11-22 17:40:18 +0100

    up

--- Filament/Resources/BaseProfileResource/Pages/ListProfiles.php
+++ Filament/Resources/BaseProfileResource/Pages/ListProfiles.php
@@ -138,6 +138,69 @@ class ListProfiles extends XotBaseListRecords
         ];
     }
 
+    public function table(Table $table): Table
+    {
+        return $table
+            // ->query($this->getTableQuery())
+            ->actions($this->getTableActions())
+            // ->actionsColumnLabel($this->getTableActionsColumnLabel())
+            // ->checkIfRecordIsSelectableUsing($this->isTableRecordSelectable())
+
+            // ->columns($this->getTableColumns())
+            ->columns($this->layoutView->getTableColumns())
+            ->contentGrid($this->layoutView->getTableContentGrid())
+            ->headerActions($this->getTableHeaderActions())
+
+            // ->columnToggleFormColumns($this->getTableColumnToggleFormColumns())
+            // ->columnToggleFormMaxHeight($this->getTableColumnToggleFormMaxHeight())
+            // ->columnToggleFormWidth($this->getTableColumnToggleFormWidth())
+            // ->content($this->getTableContent())
+            // ->contentFooter($this->getTableContentFooter())
+            // ->contentGrid($this->getTableContentGrid())
+            // ->defaultSort($this->getDefaultTableSortColumn(), $this->getDefaultTableSortDirection())
+            // ->deferLoading($this->isTableLoadingDeferred())
+            // ->description($this->getTableDescription())
+            // ->deselectAllRecordsWhenFiltered($this->shouldDeselectAllRecordsWhenTableFiltered())
+            // ->emptyState($this->getTableEmptyState())
+            // ->emptyStateActions($this->getTableEmptyStateActions())
+            // ->emptyStateDescription($this->getTableEmptyStateDescription())
+            // ->emptyStateHeading($this->getTableEmptyStateHeading())
+            // ->emptyStateIcon($this->getTableEmptyStateIcon())
+            ->filters($this->getTableFilters())
+            // ->filtersFormMaxHeight($this->getTableFiltersFormMaxHeight())
+            // ->filtersFormWidth($this->getTableFiltersFormWidth())
+            // ->groupedBulkActions($this->getTableBulkActions())
+            ->bulkActions($this->getTableBulkActions());
+        // ->header($this->getTableHeader())
+        // ->headerActions($this->getTableHeaderActions())
+        // ->modelLabel($this->getTableModelLabel())
+        // ->paginated($this->isTablePaginationEnabled())
+        // ->paginatedWhileReordering($this->isTablePaginationEnabledWhileReordering())
+        // ->paginationPageOptions($this->getTableRecordsPerPageSelectOptions())
+        // ->persistFiltersInSession($this->shouldPersistTableFiltersInSession())
+        // ->persistSearchInSession($this->shouldPersistTableSearchInSession())
+        // ->persistColumnSearchesInSession($this->shouldPersistTableColumnSearchInSession())
+        // ->persistSortInSession($this->shouldPersistTableSortInSession())
+        // ->pluralModelLabel($this->getTablePluralModelLabel())
+        // ->poll($this->getTablePollingInterval())
+        // ->recordAction($this->getTableRecordActionUsing())
+        // ->recordClasses($this->getTableRecordClassesUsing())
+        // ->recordTitle(fn (Model $record): ?string => $this->getTableRecordTitle($record))
+        // ->recordUrl($this->getTableRecordUrlUsing())
+        // ->reorderable($this->getTableReorderColumn())
+        // ->selectCurrentPageOnly($this->shouldSelectCurrentPageOnly())
+        // ->striped($this->isTableStriped())
+    }
+
+
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
     protected function getTableActions(): array
     {
         return [
